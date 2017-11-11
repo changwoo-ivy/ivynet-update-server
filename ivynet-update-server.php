d244824dcf1311005aef5ac627e35d560b85317b
changwoo <ep6tri@hotmail.com>
2017-11-12 04:45:17 +0900

    IUS_Launcher 활용한 메인 파일 완성.

--- ivynet-update-server.php
+++ ivynet-update-server.php
@@ -1,8 +1,20 @@
 <?php
 /**
  * Plugin Name: Ivynet Update Server
+ * Description: Keep projects plugins updated!
  * Version:     0.0.0
  * Author:      Changwoo Nam
  * Author URI:  https://blog.changwoo.pe.kr/
  * License:     GPLv2+
  */
+
+define( 'IUS_MAIN', __FILE__ );
+define( 'IUS_DIR', __DIR__ );
+define( 'IUS_VERSION', '0.0.0' );
+
+require_once __DIR__ . '/src/class-ius-launcher.php';
+
+$ius = new IUS_Launcher();
+$ius->launch();
+
+$GLOBALS['ius'] = $ius;
