7a57ea6c70251f08bc3e2b866117b898e91d1b0d
changwoo <ep6tri@hotmail.com>
2017-11-18 22:01:40 +0900

    버전 수정. 버전 히스토리 추가.

--- ivynet-update-server.php
+++ ivynet-update-server.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Ivynet Update Server
  * Description: Keep projects plugins updated!
- * Version:     0.0.0
+ * Version:     1.0.0
  * Author:      Ivynet
  * Author URI:  https://ivynet.kr
  * License:     GPLv2+
@@ -10,7 +10,7 @@
 
 define( 'IUS_MAIN', __FILE__ );
 define( 'IUS_DIR', __DIR__ );
-define( 'IUS_VERSION', '0.0.0' );
+define( 'IUS_VERSION', '1.0.0' );
 
 require_once __DIR__ . '/src/class-ius-launcher.php';
 
